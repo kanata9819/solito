3ef24e1eb560858919b7862425cd26f40e657a57
Sola <taka9819@gmail.com>
2026-05-12 22:13:52 +0900

    chore: Create caretUniform struct

--- solito-renderer/src/state/context.rs
+++ solito-renderer/src/state/context.rs
@@ -43,16 +43,16 @@ impl State {
         let (caret_x, caret_y, caret_w, caret_h) = self.terminal_view.caret_rect();
         let caret_color: [f32; 4] = self.terminal_view.caret_color();
 
-        rect::RectPipeline::update_caret_uniform(
-            &self.render_resources.uniform_buffer,
-            &self.gpu.queue,
-            self.window_surface.config.width,
-            self.window_surface.config.height,
+        rect::RectPipeline::update_caret_uniform(rect::CaretUniform {
+            uniform_buffer: &self.render_resources.uniform_buffer,
+            queue: &self.gpu.queue,
+            width: self.window_surface.config.width,
+            height: self.window_surface.config.height,
             caret_x,
             caret_y,
             caret_w,
             caret_h,
             caret_color,
-        );
+        });
     }
 }
